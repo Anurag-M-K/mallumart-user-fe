01390dbd40818b29b2ff50cd09e3e4b7bd95f4ee
Anurag M K <109131413+Anurag-M-K@users.noreply.github.com>
2024-07-05 15:35:24 +0530

    carousal updation

--- src/components/ui/carousel.tsx
+++ src/components/ui/carousel.tsx
@@ -206,7 +206,7 @@
         variant={variant}
         size={size}
         className={cn(
-          "absolute  h-8 w-8 rounded-full",
+          "absolute hidden md:flex  h-8 w-8 rounded-full",
           orientation === "horizontal"
             ? "-left-12 top-1/2 -translate-y-1/2"
             : "-top-12 left-1/2 -translate-x-1/2 rotate-90",
@@ -235,7 +235,7 @@
         variant={variant}
         size={size}
         className={cn(
-          "absolute h-8 w-8 rounded-full",
+          "absolute hidden md:flex h-8 w-8 rounded-full",
           orientation === "horizontal"
             ? "-right-12 top-1/2 -translate-y-1/2"
             : "-bottom-12 left-1/2 -translate-x-1/2 rotate-90",
